14b5bf9995a7b16af0ad22c03d239323d8ba63c7
ZabeenaSherzoie <zabeenasherzoie@gmail.com>
2022-09-08 06:46:06 +0000

    added selectors and event listeners for calling function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+//selectors
 const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+// Event Listeners
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 //Date 
 let today = Date.now();
 let todayDate = new Date(today);
